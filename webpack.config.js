ddbfffcc2bc231001c6fb8ce7c2095af7aee1733
Do Dang Huy <huy.dodang27@gmail.com>
2023-05-13 09:00:19 +0700

    Set up basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map', 
+    devServer: { 
+        static: './dist', 
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html', 
+            template: './src/index.html'
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: { 
+        runtimeChunk: 'single',
+    },
+};
