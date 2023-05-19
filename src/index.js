7d0747c4ae9c285bfb2ef0710397cb043525bc50
Do Dang Huy <huy.dodang27@gmail.com>
2023-05-19 20:00:01 +0700

    Use prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 "use strict";
 
 class ToDoItem {
-    constructor(title, description = "", dueDate, priority, note = "",
-        finished = false) {
+    constructor(
+        title,
+        description = "",
+        dueDate,
+        priority,
+        note = "",
+        finished = false
+    ) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
@@ -18,14 +24,14 @@ class ToDoList {
     }
 
     add(...items) {
-        items.forEach(item => {
+        items.forEach((item) => {
             if (item instanceof ToDoItem) this.list.push(item);
             else return;
         });
     }
 
     remove(...items) {
-        items.forEach(item => {
+        items.forEach((item) => {
             let index = this.list.indexOf(item);
             if (this.list[item] == -1) return;
             this.list.splice(index, 1);
@@ -45,8 +51,20 @@ const appLogic = () => {
 
     // Can use date-fns: date = format(new Date(), 'yyyy-MM-dd');
     const current = new Date();
-    const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09",
-        "10", "11", "12"];
+    const months = [
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12",
+    ];
     const month = months[current.getMonth()];
     const date = `${current.getFullYear()}-${month}-${current.getDate()}`;
 
@@ -78,9 +96,9 @@ const appLogic = () => {
         createList,
         createTask,
         finishTask,
-        removeTask
-    }
-}
+        removeTask,
+    };
+};
 
 const interactDOM = () => {
     const app = document.querySelector("#app");
@@ -100,16 +118,14 @@ const interactDOM = () => {
                     if (type !== undefined && value !== undefined) {
                         newElement.setAttribute(`${type}`, `${value}`);
                         return setAttr;
-                    }
-                    else return newElement;
-                }
-            }
-            else return newElement;
-        }
+                    } else return newElement;
+                };
+            } else return newElement;
+        };
     }
 
     function displayTask(list) {
-        document.querySelectorAll("#app > .list").forEach(element => {
+        document.querySelectorAll("#app > .list").forEach((element) => {
             element.classList.add("hidden");
         });
         let pickedList = document.querySelector(`.list[data-list='${list}']`);
@@ -122,15 +138,21 @@ const interactDOM = () => {
         }
 
         let i = 0;
-        document.querySelectorAll(".list").forEach(element => {
+        document.querySelectorAll(".list").forEach((element) => {
             element.setAttribute("data-list", i);
             i++;
         });
 
         for (let listObject of listPanel) {
             for (let task of listObject.list) {
-                addTask(task.title, task.dueDate, task.priority,
-                    task.description, task.note, listPanel.indexOf(listObject));
+                addTask(
+                    task.title,
+                    task.dueDate,
+                    task.priority,
+                    task.description,
+                    task.note,
+                    listPanel.indexOf(listObject)
+                );
             }
         }
         resetDataTaskIndex();
@@ -138,33 +160,45 @@ const interactDOM = () => {
         displayTask(currentList);
     }
 
-
     function addList() {
         // list.Panel.length has already been updated -> minus 1
         makeElement(".list")(app)("data-list", listPanel.length - 1);
         listScroller();
     }
 
-    function addTask(title, dueDate, priority, description, note, list = currentList) {
+    function addTask(
+        title,
+        dueDate,
+        priority,
+        description,
+        note,
+        list = currentList
+    ) {
         let pickedList = document.querySelector(`.list[data-list="${list}"]`);
         let currentTaskOrder = listPanel[currentList].list.length;
 
         // List name:
         if (pickedList.textContent == "") {
             let listName = pickedList.getAttribute("name");
-            listName = listName != undefined ? 
-                listName : pickedList.getAttribute("data-list");
+            listName =
+                listName != undefined ? listName : pickedList.getAttribute("data-list");
             let nameDisplay = makeElement(".list-name", "span")(pickedList);
             nameDisplay().textContent = `List ${listName}: `;
         }
 
-        let DOMTask = makeElement(".task")(pickedList)("data-task", currentTaskOrder);
+        let DOMTask = makeElement(".task")(pickedList)(
+            "data-task",
+            currentTaskOrder
+        );
 
-        DOMTask().textContent =
-            `TITLE: ${title} | DUE DATE: ${dueDate} | PRIORITY: ${priority}   `;
+        DOMTask().textContent = `TITLE: ${title} | DUE DATE: ${dueDate} | PRIORITY: ${priority}   `;
 
-        let priorityColor = priority == "High" ? "pink"
-            : (priority == "Medium" ? "yellow" : "yellowgreen");
+        let priorityColor =
+            priority == "High"
+                ? "pink"
+                : priority == "Medium"
+                    ? "yellow"
+                    : "yellowgreen";
         DOMTask().setAttribute("style", `background-color: ${priorityColor}`);
 
         let btnContainer = makeElement(".button-container")(DOMTask());
@@ -174,7 +208,10 @@ const interactDOM = () => {
         // Remove Btn
         addButton(".remove", btnContainer());
 
-        let details = makeElement(".detail")(pickedList)("data-task", currentTaskOrder);
+        let details = makeElement(".detail")(pickedList)(
+            "data-task",
+            currentTaskOrder
+        );
         details().classList.add("hidden");
         details().innerText = `DESCRIPTION: ${description}\nNOTE: ${note}`;
         DOMTask().addEventListener("mouseover", () => {
@@ -188,28 +225,30 @@ const interactDOM = () => {
     function addButton(identifier, parent) {
         const btn = makeElement(identifier, "button")(parent);
         // span.task > div.button-container > button.remove
-        const targetedTaskIndex = Number(btn().parentNode
-            .parentNode.getAttribute("data-task"));
+        const targetedTaskIndex = Number(
+            btn().parentNode.parentNode.getAttribute("data-task")
+        );
         if (identifier == ".remove") {
             btn().textContent = "X";
             btn().addEventListener("click", () => {
                 btn().parentNode.parentNode.remove();
-                document.querySelector
-                    (`.detail[data-task='${targetedTaskIndex}']`).remove();
+                document
+                    .querySelector(`.detail[data-task='${targetedTaskIndex}']`)
+                    .remove();
                 appLogic().removeTask(targetedTaskIndex);
                 resetDataTaskIndex();
             });
         } else {
-            btn().innerHTML = "&#10004;"
+            btn().innerHTML = "&#10004;";
             btn().addEventListener("click", () => {
-                btn().classList.toggle("done")
-                appLogic().finishTask(targetedTaskIndex)
+                btn().classList.toggle("done");
+                appLogic().finishTask(targetedTaskIndex);
             });
         }
     }
 
     function resetDataTaskIndex() {
-        document.querySelectorAll(".list").forEach(element => {
+        document.querySelectorAll(".list").forEach((element) => {
             if (element.hasChildNodes()) {
                 let children = element.childNodes;
                 let u = 0;
@@ -224,39 +263,48 @@ const interactDOM = () => {
     }
 
     function listScroller() {
-        const allScrollerItem = document.querySelectorAll(".scroller-container > div");
-        allScrollerItem.forEach(element => element.remove());
+        const allScrollerItem = document.querySelectorAll(
+            ".scroller-container > div"
+        );
+        allScrollerItem.forEach((element) => element.remove());
 
         for (let list of listPanel) {
-            const scrollerItem = makeElement(".scroller-item")
-                (document.querySelector(".scroller-container"))
-                ("data-list", listPanel.indexOf(list));
+            const scrollerItem = makeElement(".scroller-item")(
+                document.querySelector(".scroller-container")
+            )("data-list", listPanel.indexOf(list));
 
             let listNameIndex = Number(scrollerItem().getAttribute("data-list"));
 
-            let listName = document.querySelector(`.list[data-list='${listNameIndex}']`);
+            let listName = document.querySelector(
+                `.list[data-list='${listNameIndex}']`
+            );
             if (listName) listName = listName.getAttribute("name");
-            scrollerItem().textContent = listName != undefined ?
-                listName : listNameIndex;
+            scrollerItem().textContent =
+                listName != undefined ? listName : listNameIndex;
 
-            makeElement(".image", "img")(scrollerItem())("src", "./images/folder.svg");
+            makeElement(".image", "img")(scrollerItem())(
+                "src",
+                "./images/folder.svg"
+            );
 
             scrollerItem().addEventListener("click", () => {
-                document.querySelectorAll(".scroller-item").forEach(element => {
+                document.querySelectorAll(".scroller-item").forEach((element) => {
                     element.classList.remove("selected");
-                })
+                });
                 scrollerItem().classList.toggle("selected");
                 currentList = listNameIndex;
                 displayTask(currentList);
             });
         }
 
-        let selecedList = document.querySelector
-            (`.scroller-item[data-list='${currentList}']`);
+        let selecedList = document.querySelector(
+            `.scroller-item[data-list='${currentList}']`
+        );
         selecedList.classList.add("selected");
 
-        let moreList = makeElement(".more-list")
-            (document.querySelector(".scroller-container"));
+        let moreList = makeElement(".more-list")(
+            document.querySelector(".scroller-container")
+        );
 
         moreList().textContent = "+";
         moreList().addEventListener("click", () => {
@@ -269,7 +317,7 @@ const interactDOM = () => {
         warning.textContent = "";
         if (!document.querySelector("#title").value) {
             warning.textContent = "*You have to enter a Title!";
-        } else warning.textContent = "*Due Date has to be later than Today!"
+        } else warning.textContent = "*Due Date has to be later than Today!";
     }
 
     return {
@@ -278,16 +326,14 @@ const interactDOM = () => {
         addTask,
         listScroller,
         warning,
-        refreshAllTasks
-    }
-}
-
-
+        refreshAllTasks,
+    };
+};
 
 function restoreFromLocal() {
-    const restoredData = JSON.parse(localStorage.getItem('savedData'));
+    const restoredData = JSON.parse(localStorage.getItem("savedData"));
 
-    if (!restoredData) return listPanel = [];
+    if (!restoredData) return (listPanel = []);
     else {
         for (let listObj of restoredData) {
             listPanel.push(new ToDoList());
@@ -305,18 +351,17 @@ function restoreFromLocal() {
     // Default list
     if (listPanel.length == 0) appLogic().createList();
 
-    interactDOM().makeElement("#reset", "input")(inputArea)
-        ("type", "reset")("style", "visibility: hidden; position: absolute;");
+    interactDOM().makeElement("#reset", "input")(inputArea)("type", "reset")(
+        "style",
+        "visibility: hidden; position: absolute;"
+    );
 
     document.querySelector(".addTask").addEventListener("click", (e) => {
         e.preventDefault();
         appLogic().createTask();
-        // document.querySelector("#reset").click();
-        localStorage.setItem('savedData', JSON.stringify(listPanel));
+        document.querySelector("#reset").click();
+        localStorage.setItem("savedData", JSON.stringify(listPanel));
     });
 
     interactDOM().refreshAllTasks();
 })();
-
-
-
